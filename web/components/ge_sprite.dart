10d14bf458ef3eab7ce54853523db82a19190628
cantidio <aniquilatorbloody@gmail.com>
2014-12-05 18:27:09 -0200

    redesign of sprite component.

--- web/components/ge_sprite.dart
+++ web/components/ge_sprite.dart
@@ -1,43 +1,38 @@
-import 'dart:html';
 import 'package:polymer/polymer.dart';
 import 'package:gorgon/gorgon.dart';
 /**
- * A Polymer Draggable element.
+ * A Polymer GESprite element.
  */
 @CustomTag('ge-sprite')
 class GESprite extends PolymerElement {
   Sprite _sprite;
-  Display _display;
   Point2D _position;
 
   @published Point2D get position => _position;
 
+  @published Sprite get sprite => _sprite;
+
   @published void set position(Point2D position) {
     _position = position;
-    redraw();
+    this.fire('change');
   }
 
   @published void set offset(Point2D offset) {
     _sprite.offset = offset;
-    redraw();
+    this.fire('change');
   }
-
-  @published Sprite get sprite => _sprite;
 
   @published void set sprite(Sprite sprite) {
     _sprite = sprite;
-    _display = new Display(this.shadowRoot.children.last, width: this.parent.clientWidth, height: this.parent.clientWidth);
-    redraw();
+    this.fire('change');
   }
 
   GESprite.created() : super.created() {
+    _sprite = new Sprite();
     _position = new Point2D.zero();
   }
 
-  void redraw() {
-    _display.setAsTarget();
-    _display.clear();
+  void draw(Point2D position) {
     _sprite.draw(position);
   }
-
 }
